f6c31c5b67e568238b74f58e960f034560f850fb
42Paolo <emailcreatasulmomentoo@gmail.com>
2025-10-29 19:09:31 +0100

    fatto

--- COMMON CORE/libft/libft/ft_lstdelone.c
+++ COMMON CORE/libft/libft/ft_lstdelone.c
@@ -1,24 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pabrogi <pabrogi@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/29 11:28:40 by pabrogi           #+#    #+#             */
-/*   Updated: 2025/10/29 19:04:13 by pabrogi          ###   ########.fr       */
+/*   Created: 2025/10/29 19:08:53 by pabrogi           #+#    #+#             */
+/*   Updated: 2025/10/29 19:09:10 by pabrogi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	void	*tmp;
-
-	tmp = malloc(count * size);
-	if (!tmp)
-		return (NULL);
-	ft_bzero(tmp, count * size);
-	return (tmp);
-}
