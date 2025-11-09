61261b651535089f1ee40a278570f3313f2a0e0f
42Paolo <emailcreatasulmomentoo@gmail.com>
2025-11-09 19:38:32 +0100

    fatto

---
+++ COMMON CORE/libft/ft_split.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabrogi <pabrogi@student.42firenze.it>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/09 19:38:12 by pabrogi           #+#    #+#             */
+/*   Updated: 2025/11/09 19:38:24 by pabrogi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	count_words(const char *s, char c)
+{
+	size_t	count;
+
+	count = 0;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+			count++;
+		while (*s && *s != c)
+			s++;
+	}
+	return (count);
+}
+
+static void	free_all(char **arr, size_t i)
+{
+	while (i > 0)
+		free(arr[--i]);
+	free(arr);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char		**arr;
+	size_t		i;
+	const char	*start;
+
+	if (!s)
+		return (NULL);
+	arr = malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!arr)
+		return (NULL);
+	i = 0;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+		{
+			start = s;
+			while (*s && *s != c)
+				s++;
+			if (!(arr[i++] = ft_substr(start, 0, s - start)))
+				return (free_all(arr, i - 1), NULL);
+		}
+	}
+	arr[i] = NULL;
+	return (arr);
+}
---
+++ COMMON CORE/libft/ft_strjoin.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabrogi <pabrogi@student.42firenze.it>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/09 19:08:34 by pabrogi           #+#    #+#             */
+/*   Updated: 2025/11/09 19:14:27 by pabrogi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stddef.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+	
+	i = 0;
+	while(s[i])
+		i++;
+	return (i);	
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t				len;
+	char				*str;
+	char				*ret;
+	const unsigned char	*ptr;
+
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = (char *)malloc(len);
+	if (!str)
+		return (NULL);
+	ret = str;
+	ptr = (const unsigned char *)s1;
+	while (*ptr)
+		*str++ = *ptr++;
+	ptr = (const unsigned char *)s2;
+	while (*ptr)
+		*str++ = *ptr++;
+	*str = '\0';
+	return (ret);
+}
